b4622062d61aea73e93bdfb9dc04150480ac2e8d
Edward Huang <edwardgunawan880@yahoo.com>
2018-03-14 16:58:06 +0800

    Adding Dockerrun.aws.json

---
+++ Services/WebServices/tests/app.test.js
@@ -0,0 +1,9 @@
+let mocha = require('mocha');
+let chai = require('chai');
+let expect = chai.expect;
+let should = chai.should();
+
+
+describe('app.js', () => {
+  it('should say hello from front end');
+});
